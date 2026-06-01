f77f6568a68db8354b84ab6a54c822bf5b85ad32
Jaegu-Kang <rkdworn8@gmail.com>
2026-06-01 17:25:33 +0900

    feat(product): Querydsl 연동을 위한 Custom Repository 생성 및 병합
    동적 필터링 쿼리 명세를 위한 ProductJpaRepositoryCustom
    인터페이스 생성 및 기존 ProductJpaRepository에
    다중 상속 적용하여 단일 리포지토리로 통합

--- src/main/java/nbc/c1oud_mall/product/infrastructure/ProductJpaRepository.java
+++ src/main/java/nbc/c1oud_mall/product/infrastructure/ProductJpaRepository.java
@@ -3,5 +3,5 @@ package nbc.c1oud_mall.product.infrastructure;
 import nbc.c1oud_mall.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+public interface ProductJpaRepository extends JpaRepository<Product, Long>, ProductJpaRepositoryCustom {
 }
