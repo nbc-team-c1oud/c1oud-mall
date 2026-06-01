11175cd59d29c7128a05d62381f80b21edcf6fb7
jihyeon <bjh20049@naver.com>
2026-06-01 17:12:52 +0900

    refactor: @EnableJpaAuditing 어노테이션 추가

--- src/main/java/nbc/c1oud_mall/C1oudMallApplication.java
+++ src/main/java/nbc/c1oud_mall/C1oudMallApplication.java
@@ -2,8 +2,10 @@ package nbc.c1oud_mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class C1oudMallApplication {
 
 	public static void main(String[] args) {
