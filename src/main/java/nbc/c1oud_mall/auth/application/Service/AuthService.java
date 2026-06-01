757e4cf5d5ac54bbf7ab7139dffb1c393775e0c5
jihyeon <bjh20049@naver.com>
2026-06-01 16:13:18 +0900

    feat: AuthService에 회원가입, 로그인 로직 추가

---
+++ src/main/java/nbc/c1oud_mall/auth/application/Service/AuthService.java
@@ -0,0 +1,66 @@
+package nbc.c1oud_mall.auth.application.Service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import nbc.c1oud_mall.auth.domain.entity.User;
+import nbc.c1oud_mall.auth.infrastructure.UserRepository;
+import nbc.c1oud_mall.auth.presentation.dto.LoginRequest;
+import nbc.c1oud_mall.auth.presentation.dto.LoginResponse;
+import nbc.c1oud_mall.auth.presentation.dto.SignupRequest;
+import nbc.c1oud_mall.common.jwt.JwtUtil;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtUtil jwtUtil;
+
+	/**
+	 * 회원가입
+	 *
+	 * @param request 회권가입 정보 (이메일, 비밀번호, 이름, 핸드폰번호)
+	 */
+	@Transactional
+	public void signup(SignupRequest request) {
+		// 이메일 중복 체크
+		if (userRepository.existsByEmail(request.getEmail())) {
+			throw new RuntimeException("EMAIL_DUPLICATE");
+		}
+
+		// 비밀번호 암호화
+		String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+		// 유저 생성 후 저장
+		User user = new User(
+			request.getEmail(),
+			encodedPassword,
+			request.getName(),
+			request.getPhoneNumber()
+		);
+		userRepository.save(user);
+	}
+
+
+	@Transactional(readOnly = true)
+	public LoginResponse login(LoginRequest request) {
+		//이메일로 유저 조회
+		User user = userRepository. findByEmail(request.getEmail())
+			.orElseThrow(() -> new RuntimeException("INVALID_CREDENTIALS"));
+
+		//비밀번호 검증
+		if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+			throw new RuntimeException("INVALID_CREDENTIALS");
+		}
+
+		//JWT 토큰 발급
+		String token = jwtUtil.generateToken(user.getId(), user.getEmail(), user.getRole());
+
+		return new LoginResponse(token, "Bearer");
+	}
+
+
+}
