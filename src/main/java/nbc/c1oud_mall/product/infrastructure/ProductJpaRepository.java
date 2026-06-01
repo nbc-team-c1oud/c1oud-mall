a609cdc6cba7af0189a19a184fc8e9744432cd43
Jaegu-Kang <rkdworn8@gmail.com>
2026-06-01 15:18:15 +0900

    feat(product): 상품 JpaRepository 추가
    인프라 계층에 ProductJpaRepository 인터페이스 세팅

---
+++ src/main/java/nbc/c1oud_mall/product/infrastructure/ProductJpaRepository.java
@@ -0,0 +1,7 @@
+package nbc.c1oud_mall.product.infrastructure;
+
+import nbc.c1oud_mall.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+}
