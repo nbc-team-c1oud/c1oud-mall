c03fcf154d309056979da982bfd4af5fee9852fb
jihyeon <bjh20049@naver.com>
2026-06-01 12:17:10 +0900

    feat: UserRepository 추가
    회원가입 시 이메일 중복 체크 기능 추가
    
    로그인 시 이메일로 사용자 조회 기능 추가

---
+++ src/main/java/nbc/c1oud_mall/auth/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package nbc.c1oud_mall.auth.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nbc.c1oud_mall.auth.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	//회원가입 - 이메일 중복체크
+	boolean existsByEmail(String email);
+
+	//로그인 - 이메일로 유저 조회
+	Optional<User> findByEmail(String email);
+}
