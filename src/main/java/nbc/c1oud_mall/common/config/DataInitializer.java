c64139dceebb9fe420cff914fde775ff1e958b9d
jihyeon <bjh20049@naver.com>
2026-06-08 12:38:33 +0900

    add: 실행시 superAdmin 자동생성 로직 추가

---
+++ src/main/java/nbc/c1oud_mall/common/config/DataInitializer.java
@@ -0,0 +1,41 @@
+package nbc.c1oud_mall.common.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import nbc.c1oud_mall.auth.domain.UserRole;
+import nbc.c1oud_mall.auth.domain.entity.User;
+import nbc.c1oud_mall.auth.infrastructure.UserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	@Value("${super-admin.email}")
+	private String superAdminEmail;
+
+	@Value("${super-admin.password}")
+	private String superAdminPassword;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		if (userRepository.existsByEmail(superAdminEmail)){
+			return;
+		}
+		User superAdmin = new User(
+			superAdminEmail,
+			passwordEncoder.encode(superAdminPassword),
+			"슈퍼어드민",
+			"010-0000-0000",
+			UserRole.SUPER_ADMIN
+		);
+		userRepository.save(superAdmin);
+	}
+}
