22e523283cdad88dd5dde7db987b2352e187d9ad
jihyeon <bjh20049@naver.com>
2026-06-08 15:18:56 +0900

    fefactor: 이미 승격된 권한 예외처리

--- src/main/java/nbc/c1oud_mall/auth/application/Service/AdminService.java
+++ src/main/java/nbc/c1oud_mall/auth/application/Service/AdminService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import nbc.c1oud_mall.auth.domain.UserRole;
 import nbc.c1oud_mall.auth.domain.entity.User;
 import nbc.c1oud_mall.auth.infrastructure.UserRepository;
 import nbc.c1oud_mall.auth.presentation.dto.UserResponse;
@@ -20,15 +21,25 @@ public class AdminService {
 
 	/**
 	 * 권한 승격
-	 * @param userId 해당 id의 유저를 권한을 어드미으로 승격한다. (이미 admin인 경우 예외처리 필요?)
+	 * @param userId 해당 id의 유저를 권한을 어드민으로 승격한다.
 	 */
 	@Transactional
 	public void promoteToAdmin(Long userId) {
 		User user = userRepository.findById(userId)
 			.orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+		if (user.getRole() == UserRole.ADMIN || user.getRole() == UserRole.SUPER_ADMIN) {
+			throw new BusinessException(ErrorCode.ALREADY_ADMIN);
+		}
+
 		user.promoteToAdmin();
 	}
 
+	/**
+	 * 유저 전체 조회
+	 * @param pageable size = 20, sort = "createdAt", direction = Sort.Direction.DESC
+	 * @return 전체 유저
+	 */
 	@Transactional
 	public Page<UserResponse> getAllUsers(Pageable pageable) {
 		return userRepository.findAll(pageable)
