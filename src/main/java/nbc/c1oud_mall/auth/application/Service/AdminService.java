b0c5f4ee464aeeaad6efe17c893cc2eaab173f51
jihyeon <bjh20049@naver.com>
2026-06-08 15:07:56 +0900

    add: 유저 전체 조회 기능 추가 (권한 수정)

--- src/main/java/nbc/c1oud_mall/auth/application/Service/AdminService.java
+++ src/main/java/nbc/c1oud_mall/auth/application/Service/AdminService.java
@@ -1,11 +1,14 @@
 package nbc.c1oud_mall.auth.application.Service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import nbc.c1oud_mall.auth.domain.entity.User;
 import nbc.c1oud_mall.auth.infrastructure.UserRepository;
+import nbc.c1oud_mall.auth.presentation.dto.UserResponse;
 import nbc.c1oud_mall.common.exception.BusinessException;
 import nbc.c1oud_mall.common.exception.ErrorCode;
 
@@ -15,10 +18,20 @@ public class AdminService {
 
 	private final UserRepository userRepository;
 
+	/**
+	 * 권한 승격
+	 * @param userId 해당 id의 유저를 권한을 어드미으로 승격한다. (이미 admin인 경우 예외처리 필요?)
+	 */
 	@Transactional
 	public void promoteToAdmin(Long userId) {
 		User user = userRepository.findById(userId)
 			.orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
 		user.promoteToAdmin();
+	}
+
+	@Transactional
+	public Page<UserResponse> getAllUsers(Pageable pageable) {
+		return userRepository.findAll(pageable)
+			.map(UserResponse::from);
 	}
 }
--- src/main/java/nbc/c1oud_mall/auth/presentation/controller/AdminController.java
+++ src/main/java/nbc/c1oud_mall/auth/presentation/controller/AdminController.java
@@ -1,6 +1,11 @@
 package nbc.c1oud_mall.auth.presentation.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import nbc.c1oud_mall.auth.application.Service.AdminService;
+import nbc.c1oud_mall.auth.presentation.dto.UserResponse;
 import nbc.c1oud_mall.common.response.ApiResponse;
 import nbc.c1oud_mall.common.response.ApiResponses;
 
@@ -18,11 +24,19 @@ public class AdminController {
 
 	private final AdminService adminService;
 
+	//user 권한 승격
 	@PatchMapping("/users/{userId}/role")
 	public ResponseEntity<ApiResponse<Void>> promoteToAdmin(
 		@PathVariable Long userId) {
 		adminService.promoteToAdmin(userId);
 		return ApiResponses.noContent();
+	}
+
+	//user 전체 조회
+	@GetMapping("/users")
+	public ResponseEntity<ApiResponse<Page<UserResponse>>> getAllUsers(
+		@PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+		return ApiResponses.ok(adminService.getAllUsers(pageable));
 	}
 
 }
